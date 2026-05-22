6e2c4023feee71022d858c6b45c07f50e19d82f3
tormenask <santiagoballa2003@gmail.com>
2026-05-21 23:11:39 -0500

    feat(firebase): integrate Firebase Admin and Firestore

---
+++ virtual-classroom-backend/src/config/firebase.js
@@ -0,0 +1,19 @@
+// src/config/firebase.js
+
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = {
+  db,
+  auth,
+};
